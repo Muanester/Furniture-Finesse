852704276d39ce74a98a7cc1e1aa64ad01f2d524
Muanester <stevenmutisya999@gmail.com>
2023-10-19 01:42:08 +0300

    Category responsiveness

--- src/components/cartItems/CartItems.jsx
+++ src/components/cartItems/CartItems.jsx
@@ -8,43 +8,51 @@ function CartItems() {
     useContext(ShopContext);
   return (
     <div className="cartitems">
-      <div className="cartitems-format-main">
-        <p>Products</p>
-        <p>Title</p>
-        <p>Price</p>
-        <p>Quantity</p>
-        <p>Total</p>
-        <p>Remove</p>
-      </div>
-      <hr />
-      {all_products.map((e) => {
-        if (cartItems[e.id] > 0) {
-          return (
-            <div>
-              <div className="cartitems-format cartitems-format-main">
-                <img src={e.image} alt="" className="carticon-product-icon" />
-                <p>{e.name} </p>
-                <p>KSH. {e.new_price} </p>
-                <button className="cartitems-quantity">
-                  {" "}
-                  {cartItems[e.id]}{" "}
-                </button>
-                <p>KSH. {e.new_price * cartItems[e.id]} </p>
-                <img
-                  className="cartitems-remove-icon"
-                  onClick={() => {
-                    removeFromCart(e.id);
-                  }}
-                  src={remove_icon}
-                  alt=""
-                />
+      <div className="cartitems-scroll">
+        <div className="cartitems-format-main">
+          <p>Products</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <hr />
+        {all_products.map((e) => {
+          if (cartItems[e.id] > 0) {
+            return (
+              <div>
+                <div className="cartitems-format cartitems-format-main">
+                  <div className="cartitems-icon-div">
+                    <img
+                      src={e.image}
+                      alt=""
+                      className="carticon-product-icon"
+                    />
+                  </div>
+                  <p>{e.name} </p>
+                  <p>KSH. {e.new_price} </p>
+                  <button className="cartitems-quantity">
+                    {" "}
+                    {cartItems[e.id]}{" "}
+                  </button>
+                  <p>KSH. {e.new_price * cartItems[e.id]} </p>
+                  <img
+                    className="cartitems-remove-icon"
+                    onClick={() => {
+                      removeFromCart(e.id);
+                    }}
+                    src={remove_icon}
+                    alt=""
+                  />
+                </div>
+                <hr />
               </div>
-              <hr />
-            </div>
-          );
-        }
-        return null;
-      })}
+            );
+          }
+          return null;
+        })}
+      </div>
       <div className="cartitems-down">
         <div className="cartitems-total">
           <h1>Cart Totals</h1>
@@ -67,9 +75,9 @@ function CartItems() {
           <button>PAY NOW</button>
         </div>
         <div className="cartitems-promocode">
-          <p>If you have a promo code, enter it here</p>
+          <p>If you got from a referal, enter there name(s) here:</p>
           <div className="cartitems-promobox">
-            <input type="text" placeholder="Code" />
+            <input type="text" placeholder="Referal name" />
             <button>Submit</button>
           </div>
         </div>
